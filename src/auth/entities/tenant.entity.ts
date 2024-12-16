ccc2811ee857573367416f2a3d4ca4ac3ba4e823
lucasmelojs <141846315+lucasmelojs@users.noreply.github.com>
2024-12-16 15:33:26 -0300

    feat(auth): add tenant entity

---
+++ src/auth/entities/tenant.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity('tenants')
+export class Tenant {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  domain: string;
+
+  @Column({ default: 'active' })
+  status: string;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
+  updatedAt: Date;
+
+  @OneToMany(() => User, user => user.tenant)
+  users: User[];
+}
